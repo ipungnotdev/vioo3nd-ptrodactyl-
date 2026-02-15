0d04d14fe47a030c641233a380ab9509fdde1f2b
ipungnotdev <ipungnotdev@gmail.com>
2026-02-16 00:18:48 +0700

    Create script.js

---
+++ public/script.js
@@ -0,0 +1,6 @@
+// nanti bisa tambahkan modal QRIS atau order API
+document.querySelectorAll('.order-btn').forEach(btn=>{
+  btn.addEventListener('click',()=>{
+    alert('Klik tombol ini akan panggil API QRIS Pakasir');
+  });
+});
